66a370b2ddeaf4b0daa0aae22c1d516c6e456de8
Remok777 <roman.sadokha01@gmail.com>
2020-12-19 16:32:41 +0200

    Adding Files For Map

---
+++ EMarket/Controllers/Map.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMarket.Controllers
+{
+    public class Map : Controller
+    {
+        // GET
+        public IActionResult PointsOfProduction()
+        {
+            return View();
+        }
+    }
+}
