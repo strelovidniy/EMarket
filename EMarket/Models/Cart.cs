c164bd9b3d018bfc2da50b0a8b204e1826206f16
NazariyChetvertukha <carrrson2207@gmail.com>
2020-12-19 13:31:46 +0200

    Added new Model

---
+++ EMarket/Models/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+namespace Store.Web.Models
+{
+    public class Cart
+    {
+        public int OrderId { get; }
+
+        public int TotalCount { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public Cart(int orderId)
+        {
+            OrderId = orderId;
+            TotalCount = 0;
+            TotalPrice = 0m;
+        }
+    }
+}
---
+++ EMarket/Models/OrderModel.cs
@@ -0,0 +1,13 @@
+﻿﻿using EMarket.Models;
+ using Microsoft.AspNetCore.Http;
+
+namespace Store.Web.Models
+{
+    public class OrderModel
+    { 
+        public int Id { get; set; }
+        public  Product[] Items { get; set; }= new Product[0];
+        public  int TotalCount { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
