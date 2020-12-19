ea2533e9b158dbedfa8ed8697ab52cd4622d6ad6
TetianaLysak <lysak.tetiana22@gmail.com>
2020-12-19 12:59:55 +0200

    models product and category

---
+++ EMarket/Models/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMarket.Models
+{
+    public class AppContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //SqlConnectionStringBuilder connection = new SqlConnectionStringBuilder();
+           // connection.
+            //DbConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+        }
+    }
+}
