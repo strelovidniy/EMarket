9f45a4bdd783ae2e0779140eee1a4a8513154551
Remok777 <roman.sadokha01@gmail.com>
2020-12-19 23:01:13 +0200

    Added model validation

--- EMarket/Models/RegisterModel.cs
+++ EMarket/Models/RegisterModel.cs
@@ -8,18 +8,25 @@ namespace EMarket.Models
 {
     public class RegisterModel
     {
-        [Required(ErrorMessage = "Не указан Email")]
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Не указан пароль")]
+        [Required(ErrorMessage = "Password is less than 8 or more than 16 symbols")]
+        [MinLength(8)]
+        [MaxLength(16)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [MinLength(8)]
+        [MaxLength(16)]
         public string ConfirmPassword { get; set; }
-
+        
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
+        
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
     }
 }
