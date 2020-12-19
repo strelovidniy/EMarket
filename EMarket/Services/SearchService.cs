445b893eeecf33ecf3e367b8183b489960b3516b
NazariyChetvertukha <carrrson2207@gmail.com>
2020-12-19 22:42:06 +0200

    Moved logic to services

--- EMarket/Services/SearchService.cs
+++ EMarket/Services/SearchService.cs
@@ -1,7 +1,33 @@
-﻿namespace EMarket.Services
+﻿using System.Collections.Generic;
+using System.Linq;
+using EMarket.Models;
+
+namespace EMarket.Services
 {
-    public class SearchService
+    public class SearchService : ISearchService
     {
-        
+        public List<Product> GetAllProductsByName(string query)
+        {
+            using (var db = new AppContext())
+            {
+                return db.Products.Where(pr => pr.Name.Contains(query)).ToList();
+            }
+        }
+
+        public Product GetProductById(int id)
+        {
+            using (var db = new AppContext())
+            {
+                return db.Products.SingleOrDefault(pr => pr.Id == id);
+            }
+        }
+
+        public List<Product> GetAll()
+        {
+            using (var db = new AppContext())
+            {
+                return db.Products.ToList();
+            }
+        }
     }
 }
