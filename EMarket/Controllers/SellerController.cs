ae563d17dad1470291db9ba979650d080efe283f
TetianaLysak <lysak.tetiana22@gmail.com>
2020-12-19 18:38:06 +0200

    seller controller

---
+++ EMarket/Controllers/SellerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EMarket.Controllers
+{
+    public class SellerController : Controller
+    {
+        [Authorize(Roles = "Seller")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
