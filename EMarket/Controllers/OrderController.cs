b24a6e9092ca1b167b8e952491483f80a0c34e1f
TetianaLysak <lysak.tetiana22@gmail.com>
2020-12-20 03:48:18 +0200

    Order creation added

--- EMarket/Controllers/OrderController.cs
+++ EMarket/Controllers/OrderController.cs
@@ -10,6 +10,7 @@ using AppContext = EMarket.Models.AppContext;
 
 namespace EMarket.Controllers
 {
+    //todo
     public class OrderController : Controller
     {
         [HttpGet]
@@ -25,14 +26,64 @@ namespace EMarket.Controllers
                 products.Add(db.Products.Include(p => p.Seller)
                     .FirstOrDefaultAsync(p => p.Id == entry.Key).Result, entry.Value);
             }
+
+            foreach (var product in products)
+            {
+                if (product.Key.Count < product.Value)
+                    return View();
+            }
+
             ViewBag.Products = products;
             ViewBag.Deliveries = db.Deliveries.ToList();
+            ViewBag.TotalPrice = cart.TotalPrice;
             return View(new Order());
         }
 
         [HttpPost]
         public async Task<IActionResult> Create(Order order)
         {
+            HttpContext.Session.TryGetCart(out Cart cart);
+            if (cart == null || !cart.Items.Any())
+                return RedirectToAction("Index", "Home");
+            await using AppContext db = new AppContext();
+            var allProducts = db.Products.Where(p => cart.Items.Keys.Contains(p.Id) && p.Count >= cart.Items[p.Id]);
+            var orders = allProducts.GroupBy(key => key.SellerId).Select(p => new
+            {
+                SellerId = p.Key,
+                Products = p.Select(pr => pr)
+            });
+            foreach (var item in orders)
+            {
+                Order newOrder = (await db.Orders.AddAsync(new Order()
+                {
+                    BuyerId = order.BuyerId,
+                    DeliveryId = order.DeliveryId,
+                    Destination = order.Destination,
+                    SellerId = item.SellerId
+                })).Entity;
+
+                foreach (var product in item.Products)
+                {
+                    product.Count -= cart.Items[product.Id];
+                    order.ProductOrder.Add(new ProductOrder()
+                    {
+                        Count = cart.Items[product.Id],
+                        Price = product.Price,
+                        ProductId = product.Id,
+                        OrderId = newOrder.Id
+                    });
+                    await db.ProductOrders.AddAsync(new ProductOrder()
+                    {
+                        Count = cart.Items[product.Id],
+                        Price = product.Price,
+                        ProductId = product.Id,
+                        OrderId = newOrder.Id
+                    });
+
+                }
+                await db.SaveChangesAsync();
+            }
+
             return View();
         }
     }
